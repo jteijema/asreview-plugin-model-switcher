d74d4126767d98f30cfadd44340db71b2359000d
Jelle Teijema <j.j.teijema@gmail.com>
2021-06-17 14:12:57 +0200

    Add classifier

--- asreviewcontrib/models/SVM_LSTM.py
+++ asreviewcontrib/models/SVM_LSTM.py
@@ -2,15 +2,15 @@ from sklearn.naive_bayes import MultinomialNB
 
 from asreview.models.classifiers.base import BaseTrainClassifier
 
-class NaiveBayesDefaultParamsModel(BaseTrainClassifier):
+class SVM_LSTM_Model(BaseTrainClassifier):
     """Naive Bayes classifier
 
     The Naive Bayes classifier with the default SKLearn parameters.
     """
 
-    name = "nb_example"
+    name = "SVM_LSTM"
 
     def __init__(self):
 
-        super(NaiveBayesDefaultParamsModel, self).__init__()
+        super().__init__()
         self._model = MultinomialNB()
