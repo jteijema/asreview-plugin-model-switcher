904636c5d94a22ccbc6cfb9a5db01a2f4354e424
Jelle Teijema <j.j.teijema@gmail.com>
2021-06-17 15:49:52 +0200

    created nbnn2

--- asreviewcontrib/models/NB_NN2L.py
+++ asreviewcontrib/models/NB_NN2L.py
@@ -1,6 +1,7 @@
 from sklearn.naive_bayes import MultinomialNB
 
 from asreview.models.classifiers.base import BaseTrainClassifier
+from asreview.models.classifiers.nn_2_layer import NN2LayerClassifier
 
 class NB_NN2L_Model(BaseTrainClassifier):
     """Naive Bayes classifier
@@ -11,6 +12,25 @@ class NB_NN2L_Model(BaseTrainClassifier):
     name = "NB_NN2L"
 
     def __init__(self):
+        self.iteration = 0
 
         super().__init__()
-        self._model = MultinomialNB()
+        self.nb_model = MultinomialNB()
+        self.nn_model = NN2LayerClassifier()
+
+    def fit(self, X, y):
+        self.iteration += 1
+
+        if (self.iteration < 220) : 
+            return self.nb_model.fit(X, y)
+
+        else: 
+            self.nn_model.fit()
+
+    def predict_proba(self, X):
+        
+        if (self.iteration < 220) : 
+            return self.nb_model.predict_proba(X)
+
+        else: 
+            return self.nn_model.predict_proba(X)
