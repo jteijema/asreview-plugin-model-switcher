5b7c4385a299b2cf9d162f3560fb7aa2d9a1601d
Jelle Teijema <j.j.teijema@gmail.com>
2021-06-20 11:36:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_namespace_packages
 setup(
     name='asreview-model-switcher-extention',
     version='0.2',
-    description='The extention that adds models that switch at a certain point.',
+    description='The extention that adds models that switch at a certain set point.',
     url='https://github.com/JTeijema/ASReview-Model_Switcher',
     author='ASReview team, Jelle Teijema',
     author_email='j.j.teijema@gmail.com',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
